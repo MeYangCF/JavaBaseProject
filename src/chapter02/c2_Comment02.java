ebd03a5698699dda47d1d49f7a6d25d3f589b6a3
摆烂杨 <2971208523@qq.com>
2025-02-25 21:43:57 +0800

    更新

---
+++ src/chapter02/c2_Comment02.java
@@ -0,0 +1,12 @@
+/**
+ * @author  timerring
+ * @version  1.0
+ */
+
+package chapter02;
+
+public class c2_Comment02 {
+    //编写一个main方法
+    public static void main(String[] args) {
+    }
+}
---
+++ src/chapter02/c2_Temp2.java
@@ -0,0 +1,7 @@
+package chapter02;
+
+public class c2_Temp2 {
+    public static void main(String[] args) {
+        System.out.println("北京");
+    }
+}
