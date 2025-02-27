b21ae066852a86ee97afc01f9f9247e3ef13e346
摆烂杨 <2971208523@qq.com>
2025-02-27 15:47:53 +0800

    更新

---
+++ src/chapter04/c4_BinaryTest.java
@@ -0,0 +1,14 @@
+package chapter04;
+import java.util.Scanner;
+//
+public class c4_BinaryTest {
+
+    //编写一个main方法
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("input the name:");
+        String name = myScanner.next();
+        System.out.println(name);
+    }
+}
