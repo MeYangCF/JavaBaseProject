236badeffa7fabb4f30e3b3e46d4a7527d9dc0ae
摆烂杨 <2971208523@qq.com>
2025-02-28 15:31:36 +0800

    更新

---
+++ src/chapter05/c5_Break01.java
@@ -0,0 +1,20 @@
+package chapter05;
+
+public class c5_Break01 {
+
+	//编写一个main方法
+	public static void main(String[] args) {
+
+		//测试代码
+		for( int i = 0; i < 10; i++) {
+			if( i == 3) {
+				break ;
+			}
+			System.out.println("i=" + i);
+		}
+
+		System.out.println("退出for循环, 继续执行..");
+
+		//......
+	}
+}
---
+++ src/chapter05/c5_ForExercise02.java
@@ -0,0 +1,18 @@
+package chapter05;
+
+
+public class c5_ForExercise02 {
+
+	//编写一个main方法
+	public static void main(String[] args) {
+
+		//化繁为简
+		//先死后活
+		int n = 9;
+		for( int i = 0; i <= n; i++) {
+			System.out.println(i + "+" + (n-i) + "=" + n);
+		}
+	}
+}
+
+
