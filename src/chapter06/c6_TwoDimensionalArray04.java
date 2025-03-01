f8f9678fa2860a65facc0ee75c8c01cde84ef7a0
摆烂杨 <2971208523@qq.com>
2025-03-01 14:45:06 +0800

    更新

---
+++ src/chapter06/c6_TwoDimensionalArray04.java
@@ -0,0 +1,10 @@
+package chapter06;
+
+public class c6_TwoDimensionalArray04 {
+
+    //编写一个main方法
+    public static void main(String[] args) {
+
+        int[][] arr = {{1,1,1}, {8,8,9}, {100}};
+    }
+}
